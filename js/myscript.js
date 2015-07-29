6f7839b22774320df5208b6a1fe8dd089827d40b
Sarah Wong1 <sarahwong1@Sarahs-MacBook-Pro.local>
2015-07-28 18:11:18 -0700

    Added contact and about paragraphs

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+$('document').ready(function() {
+
+    $('#navdiv').find('a').click(function() {
+        var $href = $(this).attr('href');
+        var $anchor = $('#'+$href).offset();
+        $('body').animate({scrollTop: $anchor.top});
+        return false;
+    });
+});
